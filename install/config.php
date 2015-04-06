8bbbe7c43e456767e6243eb91d2c66d855f06ccd
刘潇 <416918875@qq.com>
2015-04-06 17:19:53 +0800

    Create config.php

---
+++ install/config.php
@@ -0,0 +1,6 @@
+<?php
+define( 'HOSTNAME' , 'localhost' );
+define( 'HOSTUSER' , 'root' );
+define( 'HOSTPASSWORD' , '' );
+define( 'HOSTDB' , 'signer' );
+define( 'ADMPSW' , 'd41d8cd98f00b204e9800998ecf8427e' );
