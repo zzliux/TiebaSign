925cb8b4bd7b7b95d23b65baf4202d365db7a801
刘潇 <416918875@qq.com>
2015-04-08 12:35:51 +0800

    Update query.php

--- query.php
+++ query.php
@@ -42,7 +42,7 @@
 			padding-top: 80px;
 			background-color: #eee;
 		}
-		.form-panel{
+		footer,.form-panel{
 			max-width: 330px;
 			margin: 0 auto;
 		}
@@ -72,4 +72,6 @@
 			</div>
 		</div>
 	</body>
+	<footer>© 2015 <a href="http://www.zzliux.tk" target="_blank">zzliux</a></footer>
+	<!-- 感谢星弦雪大神提供的BaiduUtil和登录模板 -->
 </html>
