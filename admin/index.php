fea021262c76256e306d189a2acfeae94bd91366
刘潇 <416918875@qq.com>
2015-04-20 18:11:21 +0800

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -1,135 +1,163 @@
 <?php
-	session_start();
-	if(isset($_POST['query'])){
-		header('location:../query.php?un='.$_POST['query']);
+	date_default_timezone_set('PRC');//设置北京时间
+	require_once('install/config.php');
+	require_once('BaiduUtil.php');
+	$time = date('H:i',time());
+
+	echo getTask($time);	
+	switch(getTask($time)){
+		case 'refresh': refresh(); break;
+		case 'tieba': signForTieba(20); break;
+		case 'zhidao': signForZhidao(); break;
+		case 'wenku': signForWenku(); break;
+		case 'update': update(); break;
+		default: echo 'over'; break;
 	}
-	if(isset($_POST['refresh'])){
-		header('location:../refreshTieba.php?un='.$_POST['refresh']);
-	}
-	if($_SESSION['admin'] == 1){
-		$flag = 1;
-	}else{
-		$flag = 0;
-	}
-	if(isset($_POST['psw'])){
-		require_once('../install/config.php');
-		if( md5($_POST['psw']) == ADMPSW ){
-			$flag = 1;
-			$_SESSION['admin'] = 1;
+
+	function refresh($n=5){
+		$DB=new mysqli(HOSTNAME, HOSTUSER, HOSTPASSWORD, HOSTDB);
+		if($DB->connect_errno){
+			die($DB->connect_error);
 		}
-	}
-	if(isset($_POST['logout'])){
-		$_SESSION['admin'] = 0;
-		$flag = 0;
-	}
-?>
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>贴吧签到托管-用户管理</title>
-		<meta name="viewport" charset="utf-8" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.2/css/bootstrap.min.css">
-		<style>
-		body {
-			padding-top: 10px;
-			background-color: #eee;
-		}
-		.form-panel{
-			max-width: 330px;
-			margin: 0 auto;
-		}
-		button {
-			width: 100px;
-			margin-right:0px auto;
-		}
-		</style>
-	</head>
-	<body>
-	<?php 
-		if ($flag==0){
-			echo <<<aaa
-			<div class="panel panel-primary form-panel">
-				<div class="panel-heading">用户管理</div>
-				<div class="panel-body">
-					<form class="form-horizontal" role="form" method="post">
-						<div class="form-group">
-							<label for="input_user_name" class="col-sm-3 control-label">密码</label>
-							<div class="col-sm-9">
-								<input type="password" class="form-control" name="psw" placeholder="管理员密码">
-							</div>
-						</div>
-						<div class="form-group">
-							<div class="col-sm-offset-3 col-sm-9">
-							<button type="submit" class="btn btn-primary btn-block">登录</button>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
-aaa;
-		}else{
-			require_once("../install/config.php");
-			$DB = new mysqli(HOSTNAME, HOSTUSER, HOSTPASSWORD, HOSTDB);
-			if($DB->connect_errno){
-				die($DB->connect_error);
-			}
-			$DB->query("SET NAMES utf8");
-			$sql = "select * from info";
+		$DB->query("SET NAMES utf8");
+		while($n--){
+			$sql = 'SELECT * FROM info LIMIT 1';
 			$result = $DB->query($sql);
-			echo '<div class="panel panel-primary form-panel" style="max-width:500px">
-				<div class="panel-heading">用户管理</div>
-				<div class="panel-body"><table class="table" s
-				tyle="max-width:500px;margin:0px auto;"><thead><th>#</th><th>ID</th><th>成功数</th><th>失败数</th><th>队列数</th><th>总数</th></thead>';
-			if(isset($_GET['deun'])){
-				echo "
-					<form class=\"form-horizontal\" role=\"form\" method=\"post\">
-						<span><font color=\"red\">{$_GET[deun]}</font>操作
-						<div class=\"btn-group\">
-							<button type=\"submit\" name=\"query\" value=\"{$_GET[deun]}\" class=\"btn btn-success\">签到情况</button>
-							<button type=\"submit\" name=\"refresh\" value=\"{$_GET[deun]}\" class=\"btn btn-primary\">刷新贴吧</button>
-							<button type=\"submit\" name=\"deun\" value=\"{$_GET[deun]}\" class=\"btn btn-danger\">删除</button>
-						</div>
-					</form>
-";
-			}
-			$t=0;
-			while($row = $result->fetch_assoc()){
-				$t++;
-				$sql = "select * from tieba where uid = {$row[uid]}";
-				$result_ = $DB->query($sql);
-				for($i = 0, $q = 0, $y = 0, $f = 0; $row_ = $result_->fetch_assoc();$i++){
-					if($row_['is_sign'] == 0){
-						$q++;
-					}else if($row_['is_sign']==1||$row_['is_sign']==2){
-						$y++;
-					}else{
-						$f++;
+			$row = $result->fetch_assoc();
+			$bduss = $row['bduss'];
+			$uid = $row['uid'];
+			$name = $row['un'];
+			$utl = new BaiduUtil($bduss);
+			$utl->un();
+			$result = $utl->fetchWebLikedForumList();
+			if(isset($utl->lastFetch['user']['id'])){
+				$sql = "DELETE FROM info WHERE uid = {$uid}";
+				$DB->query($sql);
+				$sql="INSERT INTO `info` (`uid`, `un`, `bduss`) VALUES ('{$uid}', '{$name}', '{$bduss}');";
+				$DB->query($sql);
+				$count = 0;
+				for($i=0;isset($result['data'][$i]);$i++){
+					$sql = "SELECT * FROM tieba WHERE tieba = '{$result['data'][$i]['forum_name']}' AND uid = {$uid}";
+					$result2 = $DB->query($sql);
+					$row = $result2->fetch_assoc();
+					if(empty($row['uid'])){
+						$sql="INSERT INTO `tieba` (`uid`, `tieba`, `is_sign`) VALUES ('{$uid}','{$result[data][$i][forum_name]}', '0');";
+						$DB->query($sql);
+						$count++;
 					}
 				}
-				echo "<tr><th>{$t}</th><th><a href=\"?deun={$row[un]}\">{$row[un]}</a></th><th>{$y}</th><th>{$f}</th><th>{$q}</th><th>{$i}</th></tr>";
 			}
-			echo '</table>';
-			echo <<<aaa
-			
-					<form class="form-horizontal" role="form" method="post">
-						<button type="submit" name="logout" class="btn btn-primary btn-block" style="width:100px">退出登录</button>
-					</form>
-aaa;
-			if(isset($_POST['deun'])){
-				$deun = $_POST['deun'];
-				$sql = "SELECT * FROM info WHERE un = '{$deun}'";
+			unset($utl);
+			echo '更新成功,用户'.$name.'新增'.$count.'个贴吧'.'<br>';
+		}
+		$DB->close();
+		return '';
+	}
+
+	function update(){
+		$DB=new mysqli(HOSTNAME, HOSTUSER, HOSTPASSWORD, HOSTDB);
+		if($DB->connect_errno){
+			die($DB->connect_error);
+		}
+		$sql = 'update tieba set is_sign = 0';
+		$DB->query($sql);
+		$DB->close();
+	}
+
+	function signForWenku(){
+		$DB=new mysqli(HOSTNAME, HOSTUSER, HOSTPASSWORD, HOSTDB);
+		if($DB->connect_errno){
+			die($DB->connect_error);
+		}
+		$DB->query("SET NAMES utf8");
+		$sql = 'select * from info';
+		$result = $DB->query($sql);
+		while($row = $result->fetch_assoc()){
+			$utl = new BaiduUtil($row['bduss']);
+			$utl->signForWenku();
+		}
+		$DB->close();
+	}
+
+	function signForZhidao(){
+		$DB=new mysqli(HOSTNAME, HOSTUSER, HOSTPASSWORD, HOSTDB);
+		if($DB->connect_errno){
+			die($DB->connect_error);
+		}
+		$DB->query("SET NAMES utf8");
+		$sql = 'select * from info';
+		$result = $DB->query($sql);
+		while($row = $result->fetch_assoc()){
+			$utl = new BaiduUtil($row['bduss']);
+			$utl->signForZhidao();
+		}
+		$DB->close();
+	}
+
+	function getTask($time){
+		$re = json_decode(file_get_contents('cronlog.php'),1);
+		foreach($re as $key => $value){
+			$t = explode(' ',$value);
+			if(strtotime($time)>=strtotime($t[0])&&strtotime($time)<=strtotime($t[1])){
+				return $key;
+			}
+		}
+	}
+
+	function signForTieba($n=15){
+		$DB=new mysqli(HOSTNAME, HOSTUSER, HOSTPASSWORD, HOSTDB);
+		if($DB->connect_errno){
+			die($DB->connect_error);
+		}
+		$DB->query("SET NAMES utf8");
+		$last=0;
+		while($n--){
+			$sql="select * from tieba where is_sign = 0 order by rand() limit 1";
+			$result = $DB->query($sql);
+			$row = $result->fetch_assoc();
+			if(($st==1||$st==4)&&$last==$row['uid']){
+				sleep(10);
+				$sql="select * from tieba where is_sign = 0 and uid = {$row[uid]} order by rand() limit 1";
 				$result = $DB->query($sql);
-				if($row = $result->fetch_assoc()){
-					$sql = "DELETE FROM info WHERE uid = $row[uid]";
-					$DB->query($sql);
-					$sql = "DELETE FROM tieba WHERE uid = $row[uid]";
-					$DB->query($sql);
-					header('location:./');
+				$row = $result->fetch_assoc();
+			}
+			$last=$row['uid'];
+			if(empty($row)){
+				$sql="select * from tieba where is_sign = 4 order by rand() limit 1";
+				$result = $DB->query($sql);
+				$row = $result->fetch_assoc();
+				if(empty($row)){
+					$sql="select * from tieba where is_sign = 3 order by rand() limit 1";
+					$result = $DB->query($sql);
+					$row = $result->fetch_assoc();
+				}else if(empty($row)){
+					return 'over';
 				}
 			}
-			echo '</div>&nbsp;&nbsp;&nbsp;&nbsp;<a href="task.php">计划任务</a></div>';
+			$sql="select * from info where uid={$row[uid]}";
+			$result_=$DB->query($sql);
+			if(empty($result_)){
+				return;
+			}
+			$row_=$result_->fetch_assoc();
+			$bduss=$row_['bduss'];
+			$utl=new BaiduUtil($bduss);
+			$re=$utl->sign($row['tieba']);
+			switch($re['status']){
+				case '0': $st=1; break;
+				case '160002': $st=2; break;
+				case '110001': $st=3; break;
+				case '340011': $st=4; break;
+				case '1':case '3':
+					$sql = "delete from tieba where uid = {$row[uid]}";
+					$DB->query($sql);
+					break;
+			}
+			$sql="update tieba set is_sign = {$st} where uid={$row[uid]} and tieba = '{$row[tieba]}'";
+			$DB->query($sql);
+			unset($utl);
 		}
-	?>
-	</body>
-	<footer style="margin:0px auto;max-width:<?php if($flag) echo '500px'; else echo '330px'; ?>">© 2015 <a href="http://www.zzliux.tk">zzliux</a></footer>
-</html>
+		$DB->close();
+		return 'ok';
+	}
+?>
